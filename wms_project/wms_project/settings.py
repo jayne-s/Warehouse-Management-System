ab38abccbc42d9046e86e8ca5f55890e1a8a4e0c
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2026-03-30 00:37:44 -0500

    Update settings.py

--- wms_project/wms_project/settings.py
+++ wms_project/wms_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$g8+$ugbrty=^t*r4((p9$2ym*fj#=*+a-^4hmfj1o@-($qi+='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
